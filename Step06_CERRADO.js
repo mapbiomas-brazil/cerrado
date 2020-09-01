f7749dca4a4903392da8c207835f5c528c8fce55
felipe-lenti <70353932+felipe-lenti@users.noreply.github.com>
2020-08-31 21:41:20 -0300

    Add files via upload

---
+++ Step06_CERRADO.js
@@ -0,0 +1,154 @@
+//User parameters
+
+
+var versao = "1";
+var bioma = "CERRADO"
+                   
+var dircol5 = 'projects/mapbiomas-workspace/COLECAO5/classificacao-test/colecao_5_cerrado_v_1'
+var dirout = 'projects/mapbiomas-workspace/COLECAO5/classificacao-test/'
+
+
+var palettes = require('users/mapbiomas/modules:Palettes.js');
+
+// var Bioma_geral = ee.FeatureCollection("users/SEEGMapBiomas/bioma_1milhao_uf2015_250mil_IBGE_geo_v4_revisao_pampa_lagoas")
+// //print(Bioma_geral)
+// var Bioma_MA = Bioma_geral.filterMetadata('CD_LEGENDA', 'equals', 'CERRADO')
+
+var vis = {
+    'min': 0,
+    'max': 34,
+    'palette': palettes.get('classification2')
+};
+
+var image = ee.Image(dircol5);
+
+var years = [
+    1985, 1986, 1987, 1988,
+    1989, 1990, 1991, 1992,
+    1993, 1994, 1995, 1996,
+    1997, 1998, 1999, 2000,
+    2001, 2002, 2003, 2004,
+    2005, 2006, 2007, 2008,
+    2009, 2010, 2011, 2012,
+    2013, 2014, 2015, 2016,
+    2017, 2018, 2019];
+
+/**
+ * User defined functions
+ */
+var applyGapFill = function (image) {
+
+    // apply the gap fill form t0 until tn
+    var imageFilledt0tn = bandNames.slice(1)
+        .iterate(
+            function (bandName, previousImage) {
+
+                var currentImage = image.select(ee.String(bandName));
+
+                previousImage = ee.Image(previousImage);
+
+                currentImage = currentImage.unmask(
+                    previousImage.select([0]));
+
+                return currentImage.addBands(previousImage);
+
+            }, ee.Image(imageAllBands.select([bandNames.get(0)]))
+        );
+
+    imageFilledt0tn = ee.Image(imageFilledt0tn);
+
+    // apply the gap fill form tn until t0
+    var bandNamesReversed = bandNames.reverse();
+
+    var imageFilledtnt0 = bandNamesReversed.slice(1)
+        .iterate(
+            function (bandName, previousImage) {
+
+                var currentImage = imageFilledt0tn.select(ee.String(bandName));
+
+                previousImage = ee.Image(previousImage);
+
+                currentImage = currentImage.unmask(
+                    previousImage.select(previousImage.bandNames().length().subtract(1)));
+
+                return previousImage.addBands(currentImage);
+
+            }, ee.Image(imageFilledt0tn.select([bandNamesReversed.get(0)]))
+        );
+
+
+    imageFilledtnt0 = ee.Image(imageFilledtnt0).select(bandNames);
+
+    return imageFilledtnt0;
+};
+
+// get band names list 
+var bandNames = ee.List(
+    years.map(
+        function (year) {
+            return 'classification_' + String(year);
+        }
+    )
+);
+
+// generate a histogram dictionary of [bandNames, image.bandNames()]
+var bandsOccurrence = ee.Dictionary(
+    bandNames.cat(image.bandNames()).reduce(ee.Reducer.frequencyHistogram())
+);
+
+print(bandsOccurrence);
+
+// insert a masked band 
+var bandsDictionary = bandsOccurrence.map(
+    function (key, value) {
+        return ee.Image(
+            ee.Algorithms.If(
+                ee.Number(value).eq(2),
+                image.select([key]).byte(),
+                ee.Image().rename([key]).byte().updateMask(image.select(0))
+            )
+        );
+    }
+);
+
+// convert dictionary to image
+var imageAllBands = ee.Image(
+    bandNames.iterate(
+        function (band, image) {
+            return ee.Image(image).addBands(bandsDictionary.get(ee.String(band)));
+        },
+        ee.Image().select()
+    )
+);
+
+// generate image pixel years
+var imagePixelYear = ee.Image.constant(years)
+    .updateMask(imageAllBands)
+    .rename(bandNames);
+
+// apply the gap fill
+var imageFilledtnt0 = applyGapFill(imageAllBands)
+                       .set("v", versao)
+                       .set("biome", "CERRADO")
+                       .set("step", "gapfill");
+
+
+Map.addLayer(image.select('classification_1985'), vis, 'image');
+
+
+Map.addLayer(imageFilledtnt0.select('classification_1985'), vis, 'filtered');
+
+print(imageFilledtnt0);
+print(dirout+'/CERRADO_'+versao)
+
+Export.image.toAsset({
+    'image': imageFilledtnt0,
+    'description': 'CERRADO_step1_'+ versao,
+    'assetId': dirout+'CERRADO_step1_'+versao,
+    'pyramidingPolicy': {
+        '.default': 'mode'
+    },
+    'region': geometry,
+    'scale': 30,
+    'maxPixels': 1e13
+});
