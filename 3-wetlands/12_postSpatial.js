51e28a18d15a1e9503c6eea08161bf01c8606cb1
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2021-08-19 09:30:47 -0300

    Update 12_postSpatial.js

--- 3-wetlands/12_postSpatial.js
+++ 3-wetlands/12_postSpatial.js
@@ -1,3 +1,6 @@
+// Post-processing - Spatial filter 
+// For clarification, write to <dhemerson.costa@ipam.org.br> and <felipe.lenti@ipam.org.br>
+
 // define geometry (raw extent of cerrado)
 var geometry = /* color: #98ff00 */ee.Geometry.Polygon(
         [[[-42.306314047317365, -1.7207103925816054],
@@ -11,57 +14,51 @@ var geometry = /* color: #98ff00 */ee.Geometry.Polygon(
           [-40.548501547317365, -17.52511660076233],
           [-40.636392172317365, -2.774571568871124]]]);
 
+// define input
 var bioma = "CERRADO";
-var dirout = 'projects/mapbiomas-workspace/COLECAO6/classificacao-test/';
 var file_in = 'CERRADO_col6_wetlands_gapfill_incid_temporal_v53';
+
+// define output
+var dirout = 'projects/mapbiomas-workspace/COLECAO6/classificacao-test/';
 var file_out = 'CERRADO_col6_wetlands_gapfill_incid_temporal_spatial_v';
 var version_out = 53;
 
 // read image
 var class4GAP = ee.Image(dirout + file_in);
 
-// speciy filter
+// define minimum mapeable area 
 var filter_lte = 6;
 
+// import mapbiomas color ramp
 var palettes = require('users/mapbiomas/modules:Palettes.js');
-var pal = palettes.get('classification2');
 var vis = {
-      bands: 'classificationWet_1985',
+      bands: 'classification_1985',
       min:0,
       max:34,
-      palette: pal,
+      palette: palettes.get('classification2'),
       format: 'png'
     };
 
-Map.addLayer(class4GAP, vis, 'class4GAP');
+// plot 
+Map.addLayer(class4GAP, vis, 'unfiltered');
 
-//var aplicaFiltroEsp = function(imagem, ano){
-//   var img_out = imagem.select('classification_1985')
-//   for (var i_ano=0;i_ano<anos5.length; i_ano++){  
-//     var ano = anos5[i_ano];  
-//     img_out = img_out.addBands(mask5(valor,ano, imagem)) } 
-//   return img_out
-//}
-
+// compute mode
 var ano = '1985'; 
-//var mask_85 = class4GAP.select('connect_'+ano)
 var moda_85 = class4GAP.select('classificationWet_'+ ano).focal_mode(1, 'square', 'pixels')
 var conn = class4GAP.select('classificationWet_'+ ano).connectedPixelCount(100,false).rename('connect_'+ano)
 moda_85 = moda_85.mask(conn.select('connect_'+ ano).lte(filter_lte))
 var class_outTotal = class4GAP.select('classificationWet_'+ano).blend(moda_85)
-//var kernel = ee.Kernel.square(1)
-//var neighs = class4GAP.select('classification_'+ano).neighborhoodToBands(kernel)//.mask(region)
-//var majority = neighs.reduce(ee.Reducer.mode()).rename('classification_'+ano)
 
-//print(class_outTotal)
+// plot mode
 Map.addLayer(class_outTotal, vis, 'class4 MODA');
 
+// define years to be filtered
+var anos = ['1986','1987','1988','1989','1990','1991', '1992','1993','1994','1995',
+            '1996','1997','1998','1999','2000','2001', '2002','2003','2004','2005',
+            '2006','2007','2008','2009','2010','2011', '2012','2013','2014','2015',
+            '2016','2017','2018', '2019', '2020'];
 
-var anos = ['1986','1987','1988','1989','1990','1991',
-'1992','1993','1994','1995','1996','1997','1998','1999','2000','2001',
-'2002','2003','2004','2005','2006','2007','2008','2009','2010','2011',
-'2012','2013','2014','2015','2016','2017','2018', '2019', '2020'];
-
+// apply spatial filter
 for (var i_ano=0;i_ano<anos.length; i_ano++){  
   var ano = anos[i_ano]; 
   var moda = class4GAP.select('classificationWet_'+ano).focal_mode(1, 'square', 'pixels')
@@ -70,11 +67,14 @@ for (var i_ano=0;i_ano<anos.length; i_ano++){
   var class_out = class4GAP.select('classificationWet_'+ano).blend(moda)
   class_outTotal = class_outTotal.addBands(class_out)
 }
+
+// print
 print(class_outTotal)
+
+// set properties
 class_outTotal = class_outTotal.set("version", version_out).set("step", "spatial")
 
-Map.addLayer(class_outTotal, vis, 'out')
-
+// export as GEE asset
 Export.image.toAsset({
     'image': class_outTotal,
     'description': file_out + version_out,
@@ -86,3 +86,4 @@ Export.image.toAsset({
     'scale': 30,
     'maxPixels': 1e13
 });
+
