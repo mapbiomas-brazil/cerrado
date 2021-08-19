1a8a94f52a8728f644b0c6a3c2bbd89501c23d7c
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2021-08-19 11:47:28 -0300

    Update 24_postFreq.js

--- 3-wetlands/24_postFreq.js
+++ 3-wetlands/24_postFreq.js
@@ -1,33 +1,40 @@
-var ano = '2019';
+// Post-processing - Frequency filter
+// For clarification, write to <dhemerson.costa@ipam.org.br> and <felipe.lenti@ipam.org.br>
 
+// define input
 var bioma = "CERRADO";
 var file_in = bioma + '_col6_wetlands_gapfill_incid_temporal_spatial_v54';
-var file_out = bioma + '_col6_wetlands_gapfill_incid_temporal_spatial_freq_v';
+
+// define output
 var dirout = 'projects/mapbiomas-workspace/COLECAO6/classificacao-test/';
+var file_out = bioma + '_col6_wetlands_gapfill_incid_temporal_spatial_freq_v';
 var version_out = 54;
 
+// read input as image
 var class4 = ee.Image(dirout + file_in);
 
+// define year to plot example
+var ano = '2019';
 
+// import mapbiomas color ramp 
 var palettes = require('users/mapbiomas/modules:Palettes.js');
-var pal = palettes.get('classification2');
 var vis = {
-      bands: 'classificationWet_'+ ano,
-      min:0,
-      max:34,
-      palette: pal,
+      bands: 'classification_'+ ano,
+      min: 0,
+      max: 49,
+      palette: palettes.get('classification6');,
       format: 'png'
     };
 
+// import collection 5.0
 var col5 = ee.Image('projects/mapbiomas-workspace/public/collection5/mapbiomas_collection50_integration_v1')
 
+// plot collection 5.0
 Map.addLayer(col5.updateMask(class4.select(['classification_' + ano])), vis, 'col5')
 
+// define the function to calc frequencies 
 var filtrofreq = function(mapbiomas){
-  ////////Calculando frequencias
-  //////////////////////
-  ////////////////////
-  // General rule
+ // General rule
  var exp = '100*((b(0)+b(1)+b(2)+b(3)+b(4)+b(5)+b(6)+b(7)+b(8)+b(9)+b(10)+b(11)+b(12)+b(13)+b(14)+b(15)' +
       '+b(16)+b(17)+b(18)+b(19)+b(20)+b(21)+b(22)+b(23)+b(24)+b(25)+b(26)+b(27)+b(28)+b(29)+b(30)+b(31)+b(32)+b(33)+b(34)+b(35))/36 )';
   
@@ -39,14 +46,10 @@ var filtrofreq = function(mapbiomas){
   var wetFreq = mapbiomas.eq(11).expression(exp);
   var grassFreq = mapbiomas.eq(12).expression(exp);
 
-  //////seleciona o que foi vegetação nativa pelo menos X% da série
-  var vegMask = ee.Image(0)
-                           .where((florFreq.add(savFreq).add(grassFreq).add(wetFreq)).gte(90), 1);
-  
-  //var NaovegMask = ee.Image(0)
-  //                         .where(agro.gt(95), 21)
-  
-  /////converte para toda série
+  // select pixels that were native vegetation at least 90% of the time series
+  var vegMask = ee.Image(0).where((florFreq.add(savFreq).add(grassFreq).add(wetFreq)).gte(90), 1);  
+
+  // apply for all the time-series when:
   var  vegMap = ee.Image(0)
                           .where(vegMask.eq(1).and(AgrFreq.gt(50)), 19)
                           .where(vegMask.eq(1).and(pastureFreq.gt(50)), 15)
@@ -55,33 +58,30 @@ var filtrofreq = function(mapbiomas){
                           .where(vegMask.eq(1).and(grassFreq.gt(50)), 12)
                           .where(vegMask.eq(1).and(savFreq.gt(50)), 4)
 
-  vegMap = vegMap.updateMask(vegMap.neq(0))//.clip(BiomaPA)
-  //NaovegMask = NaovegMask.updateMask(NaovegMask.neq(0))//.clip(BiomaPA)
-  //Map.addLayer(vegMap, vis, 'vegetacao estavel', true);
-  //Map.addLayer(NaovegMask, vis, 'Não vegetacao estavel', true);
+  // mask pixels that were not filtered
+  vegMap = vegMap.updateMask(vegMap.neq(0))
 
   var saida = mapbiomas.where(vegMap, vegMap)
-  //saida = saida.where(NaovegMask, NaovegMask)
-  
+ 
   return saida;
 }
 
-
-  
+// set properties   
   var saida = filtrofreq(class4)
   saida = saida
   .set('version', version_out)
   .set('biome', bioma)
   .set('step', 'frequency')
 
+// print 
 print(class4)
 print(saida)
 
+// plot 
 Map.addLayer(class4, vis, 'image');
-
 Map.addLayer(saida, vis, 'filtered');
 
-
+// export as GEE asset
 Export.image.toAsset({
     'image': saida,
     'description': file_out + version_out,
