e548118c6ba851f83b5d4090f7a865ea679bf4fb
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2024-03-06 09:58:21 -0300

    Update 01_computeProportion.js

--- 2-rocky-outcrop/01_computeProportion.js
+++ 2-rocky-outcrop/01_computeProportion.js
@@ -1,21 +1,22 @@
-// -- -- -- -- 01_trainingMask
-// get samples of rocky outcrop
+// -- -- -- -- 01_computeProportion
+// compute area by class to be used as reference to estimate samples of rocky outcrop
 // barbara.silva@ipam.org.br
 
-
-// // input metadata
-var version = '3';
+// input metadata
+var version = '0';
 
 // define classes to be assessed
 var classes = [3, 4, 11, 12, 15, 19, 21, 33];
 
 // output directory
-var dirout = 'users/barbarasilvaIPAM/rocky_outcrop/collection8/sample/area/';
+var dirout = 'projects/barbaracosta-ipam/assets/collection-9_rocky-outcrop/sample/area/';
 
 // area of interest for rocky outcrop
+var aoi = ee.Image(1).clip(ee.FeatureCollection('projects/ee-barbarasilvaipam/assets/collection8-rocky/masks/aoi_v5'));
+Map.addLayer (aoi, {palette: ['red']}, "Area of Interest");
 
-// stable pixels collection 6
-var stable = ee.Image('projects/ee-barbarasilvaipam/assets/collection8/masks/cerrado_stablePixels_1985_2021_v2')
+// stable pixels from collection 8
+var stable = ee.Image('users/dh-conciani/collection9/masks/cerrado_trainingMask_1985_2022_v0')
                 .updateMask(aoi.eq(1));
 
 // import the color ramp module from mapbiomas 
@@ -23,13 +24,15 @@ var palettes = require('users/mapbiomas/modules:Palettes.js');
 var vis = {
     'min': 0,
     'max': 62,
-    'palette': palettes.get('classification7')
+    'palette': palettes.get('classification8')
 };
 
-// get cerrado biome
+Map.addLayer(stable, vis, 'stable');
+
+// get cerrado biome layer
 var cerrado = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/biomas-2019')
                 .filterMetadata('Bioma', 'equals', 'Cerrado');
-                
+
 // define function to compute area (skm)
 var pixelArea = ee.Image.pixelArea().divide(1000000);
 
@@ -48,7 +51,7 @@ var getArea = function(feature) {
                                       reducer: ee.Reducer.sum(),
                                       geometry: feature.geometry(),
                                       scale: 30,
-                                      maxPixels: 1e13 }
+                                      maxPixels: 1e13}
                                     ).get('area')
                                   )
                               ); // end of set
@@ -60,11 +63,7 @@ var getArea = function(feature) {
 var computed_obj = cerrado.map(getArea);
 print (computed_obj);
 
-Map.addLayer(aoi, {palette: ['red']}, 'aoi');
-Map.addLayer(stable, vis, 'stable');
-
 // export computation as GEE asset
 Export.table.toAsset({'collection': computed_obj, 
                       'description': 'stable_v' + version,
                       'assetId': dirout + 'stable_v' + version});
-                      
