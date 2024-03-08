89c6c74df4638f2aaf1e73716d2e32e04ab0bb38
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2024-03-08 14:14:25 -0300

    Update 07_spatial.js

--- 2-rocky-outcrop/07_spatial.js
+++ 2-rocky-outcrop/07_spatial.js
@@ -2,42 +2,41 @@
 // spatial filter - minimum area
 // barbara.silva@ipam.org.br 
 
-// define root
-var root = 'projects/ee-barbarasilvaipam/assets/collection8-rocky/general-class-post/';
+// set metadata 
+var input_version = '0';
+var output_version = '0';
 
-// define input file 
-var file_in = 'CERRADO_col8_rocky_gapfill_frequency_v3';
+// set directories
+var input = 'projects/barbaracosta-ipam/assets/collection-9_rocky-outcrop/general-class-post/CERRADO_col9_rocky_gapfill_frequency_v' + input_version;
+var dirout = 'projects/barbaracosta-ipam/assets/collection-9_rocky-outcrop/general-class-post/';
 
-// define output version 
-var version_out = 4;
-
-// read image
-var classification = ee.Image(root + file_in);
-
-// define minimum mapeable area
-var filter_size = 6;
+// load classification
+var classification = ee.Image(input);
+print ("input", classification);
 
 // import mapbiomas color ramp
 var vis = {
       min:0,
       max:62,
-      palette: require('users/mapbiomas/modules:Palettes.js').get('classification7'),
+      palette: require('users/mapbiomas/modules:Palettes.js').get('classification8'),
     };
 
 // plot  input version
 Map.addLayer(classification.select(['classification_2010']), vis, 'input 2010');
 
-
 // create an empty recipe
 var filtered = ee.Image([]);
 
+// define minimum mapeable area
+var filter_size = 50;
+
 // apply filter
-ee.List.sequence({'start': 1985, 'end': 2022}).getInfo()
+ee.List.sequence({'start': 1985, 'end': 2023}).getInfo()
       .forEach(function(year_i) {
         // compute the focal model
         var focal_mode = classification.select(['classification_' + year_i])
                 .unmask(0)
-                .focal_mode({'radius': 1, 'kernelType': 'square', 'units': 'pixels'});
+                .focal_mode({'radius': 10, 'kernelType': 'square', 'units': 'pixels'});
  
         // compute te number of connections
         var connections = classification.select(['classification_' + year_i])
@@ -64,12 +63,12 @@ Map.addLayer(filtered.select(['classification_2010']), vis, 'filtered 2010 - rou
 var recipe = ee.Image([]);
 
 // apply filter
-ee.List.sequence({'start': 1985, 'end': 2022}).getInfo()
+ee.List.sequence({'start': 1985, 'end': 2023}).getInfo()
       .forEach(function(year_i) {
         // compute the focal model
         var focal_mode = filtered.select(['classification_' + year_i])
                 .unmask(0)
-                .focal_mode({'radius': 1, 'kernelType': 'square', 'units': 'pixels'});
+                .focal_mode({'radius': 10, 'kernelType': 'square', 'units': 'pixels'});
  
         // compute te number of connections
         var connections = filtered.select(['classification_' + year_i])
@@ -91,12 +90,13 @@ ee.List.sequence({'start': 1985, 'end': 2022}).getInfo()
 
 Map.addLayer(recipe.select(['classification_2010']), vis, 'filtered 2010 - round 2');
 
+print ('output', recipe);
 
 // export as GEE asset
 Export.image.toAsset({
     'image': recipe,
-    'description': 'CERRADO_col8_rocky_gapfill_frequency_spatial_v' + version_out,
-    'assetId': root + 'CERRADO_col8_rocky_gapfill_frequency_spatial_v' + version_out,
+    'description': 'CERRADO_col9_rocky_gapfill_frequency_spatial_v' + output_version,
+    'assetId': dirout + 'CERRADO_col9_rocky_gapfill_frequency_spatial_v' + output_version,
     'pyramidingPolicy': {
         '.default': 'mode'
     },
