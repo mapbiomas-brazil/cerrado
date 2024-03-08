3202bc0f5db1cf1dc6e4b3a248f9619c1aec20ac
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2024-03-08 12:27:50 -0300

    Update 05_gapFill.js

--- 2-rocky-outcrop/05_gapFill.js
+++ 2-rocky-outcrop/05_gapFill.js
@@ -2,55 +2,34 @@
 // post-processing - fill gaps (nodata) with data from previous years
 // barbara.silva@ipam.org.br
 
-// set the cerrado extent 
+// set the rocky outcrop extent 
 var geometry = ee.Geometry.Polygon (
-[[[-41.37543452290266,-3.6773987041014786],
-  [-48.273939094899134,-6.563020899300923],
-  [-48.24659702837338,-9.798906539350297],
-  [-49.46053964500549,-12.7089177827255],
-  [-50.59234499311172,-14.49329620462519],
-  [-52.64207960438195,-15.181085022681668],
-  [-57.59252323349536,-14.986692873017187],
-  [-58.242578606836716,-15.943307480893033],
-  [-57.146197800223625,-16.910053753329763],
-  [-57.141504574451865,-17.917776362571292],
-  [-57.07361219664365,-18.673685045570164],
-  [-57.35785649393868,-19.512827252680655],
-  [-58.06505073320582,-20.692941488617265],
-  [-56.61596440610372,-22.122773040137734],
-  [-54.382244055542735,-19.809042125463858],
-  [-53.69411037204978,-18.28747883455363],
-  [-52.46842999860558,-17.5502256029074],
-  [-49.34009411498674,-16.889662647042297],
-  [-48.55759401802354,-17.823527949886824],
-  [-48.028130117601854,-18.322928900186156],
-  [-47.8811414334333,-19.631023180971603],
-  [-47.7402397960028,-20.315912186663887],
-  [-47.178597239902444,-20.93821466886093],
-  [-46.420962681797846,-21.094287228232602],
-  [-45.598267298001325,-20.774496578697853],
-  [-42.99747272243816,-19.70487946455934],
-  [-42.57451633581474,-18.60373760975882],
-  [-42.24564900325984,-16.06000100035008],
-  [-42.358140276276764,-15.055560224012149],
-  [-43.07074399890996,-15.110935176172486],
-  [-43.69689718392297,-15.546162902310774],
-  [-45.15858205402829,-17.243566398567214],
-  [-45.59698997709771,-16.532881370042322],
-  [-46.7381011219985,-14.416834035054313],
-  [-44.541873088239804,-12.667054102327889],
-  [-43.61838110815923,-9.434881068955413],
-  [-46.07753196667198,-7.043265545851765],
-  [-40.66218289953771,-5.178029098641075],
-  [-41.37543452290266,-3.6773987041014786]]]);
-  
+[[[-41.89424073899461,-3.5402242777700574],
+[-48.83760011399461,-6.385663504252147],
+[-48.96943605149461,-10.557420640233374],
+[-50.81513917649461,-13.906814250754953],
+[-58.68135011399461,-14.503234080476853],
+[-58.50556886399461,-22.293613049106106],
+[-55.78095948899461,-22.374910342856523],
+[-53.18818605149461,-18.463398351122954],
+[-49.36494386399461,-17.92066736932659],
+[-47.82685792649461,-22.21226843307078],
+[-42.64131105149461,-19.99858562581205],
+[-41.76240480149461,-13.821484186840916],
+[-43.21260011399461,-13.565307050922476],
+[-43.08076417649461,-8.998421853698893],
+[-45.10224855149461,-7.214743125944735],
+[-40.26826417649461,-5.336504449649562],
+[-41.05927980149461,-3.1015101677947055],
+[-41.89424073899461,-3.5402242777700574]]]);
+
 // set metadata 
-var input_version = '3';
-var output_version = '3';
+var input_version = '0';
+var output_version = '0';
 
 // set directories
-var input = 'projects/ee-barbarasilvaipam/assets/collection8-rocky/general-class/CERRADO_col8_rocky_v3';
-var output = 'projects/ee-barbarasilvaipam/assets/collection8-rocky/general-class-post/';
+var input = 'projects/barbaracosta-ipam/assets/collection-9_rocky-outcrop/general-class/CERRADO_col9_rocky_v0';
+var dirout = 'projects/barbaracosta-ipam/assets/collection-9_rocky-outcrop/general-class-post/';
 var filename = 'CERRADO_col8_rocky_gapfill_v';
 
 // import classification 
@@ -64,13 +43,13 @@ print('input classification', image);
 var vis = {
     'min': 0,
     'max': 62,
-    'palette': require('users/mapbiomas/modules:Palettes.js').get('classification7')
+    'palette': require('users/mapbiomas/modules:Palettes.js').get('classification8')
 };
 
 Map.addLayer(image.select(['classification_2022']), vis, 'input');
 
 // set the list of years to be filtered
-var years = ee.List.sequence({'start': 1985, 'end': 2022, step: 1}).getInfo();
+var years = ee.List.sequence({'start': 1985, 'end': 2023, step: 1}).getInfo();
 
 /**
  * User defined functions
@@ -171,13 +150,13 @@ print ('output classification', imageFilledtnt0);
 Map.addLayer(imageFilledtnt0.select('classification_2022'), vis, 'filtered');
 
 // write metadata
-imageFilledtnt0 = imageFilledtnt0.set('vesion', output_version);
+imageFilledtnt0 = imageFilledtnt0.set('version', output_version);
 
 // export as GEE asset
 Export.image.toAsset({
     'image': imageFilledtnt0,
     'description': filename + output_version,
-    'assetId': output + filename + output_version,
+    'assetId': dirout + filename + output_version,
     'pyramidingPolicy': {
         '.default': 'mode'
     },
@@ -185,3 +164,4 @@ Export.image.toAsset({
     'scale': 30,
     'maxPixels': 1e13
 });
+
