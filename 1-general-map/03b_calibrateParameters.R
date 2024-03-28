5c5b0faf42970db4547154a1ffb0fef180ea7e24
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2024-03-28 16:36:14 -0300

    Update 03b_calibrateParameters.R

--- 1-general-map/03b_calibrateParameters.R
+++ 1-general-map/03b_calibrateParameters.R
@@ -48,11 +48,12 @@ getYears <- function(start_year, end_year, proportion) {
   )
 }
 
-## set grid to be tested
+## set grid of parameters to be tested
 combinations <- expand.grid(
   ntree=c(100, 200, 400, 800),
   mtry=c(5, 10, 20, 40)
 )
+
 
 ## set recipes
 paramTable <- as.data.frame(NULL)
@@ -123,170 +124,16 @@ for (i in 1:length(region_name)) {
       ## insert bandnames
       tempImportance$bandNames <- row.names(tempImportance)
       
-      
-      
-      
-      #print(trainedClassifier$explain()$getInfo())
+      ## bind data
+      paramTable <- rbind(paramTable, tempParam)
+      importanceTable <- rbind(importanceTable, tempImportance)
       
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    ## 
-    ## get spectral signatures from GEE and ingest locally 
-    print('Ingesting array of samples locally')
-    sample_ij <- as.data.frame(na.omit(ee_as_sf(samples_ij, via = 'drive')))
-    
-    ## remove description columns 
-    sample_ij <- sample_ij[ , -which(names(sample_ij) %in% c("id","mapb", "geometry"))]
-    
-    
- 
-    
-    
-    
-    
-    
-     
   }
   
 }
 
-
-## set a number of random years to be used in the estimation (20% of total)
-
-## define the number of models repetitions ito be used in heuristic search for each year 
-n_rep <- 10 # NAO PRECISA
-
-## set sub sample of proportion (%) - used to improved performance into cross-validation k-folds
-p <- 10 # nao precisa, faz com 100% 
-
- estimation
-  
-  
-  ## get spectral signatures for a random year (repeat n times)
-  for (j in 1:n_years) {
-   
-    
-   
-    
-
-    
-    ## standardize seed
-    set.seed(1)
-    
-    ## run n times
-    for (k in 1:n_rep) {
-      print(paste0('Training RF repetition ', k, ' of ', n_rep, ' (with 10 x 3 k-folds each)'))
-      
-      ## perform sub sample
-      sub_sample <- sample_ij[sample(x= 1: nrow(sample_ij),
-                                     size = round(nrow(sample_ij) / 100 * p)) ,]
-      
-      ## train model 
-      custom <- train(as.factor(reference)~.,
-                      data= sub_sample, 
-                      method= customRF, 
-                      metric= 'Accuracy', 
-                      tuneGrid= tunegrid, 
-                      trControl= control)
-      
-      ## in the first iteration, build objects
-      if (exists('tune') == FALSE) {
-        ## get best parameters
-        tune <- custom$bestTune
-        imp <- varImp(custom, scale= TRUE)$importance
-        imp$band <- row.names(imp)
-      } else {
-        ## get best parameters
-        ## add
-        tune <- rbind(tune, custom$bestTune)
-        imp_x <- varImp(custom, scale= TRUE)$importance
-        imp_x$band <- row.names(imp_x)
-        imp <- rbind(imp, imp_x)
-      }
-      
-    } ## end of sub-sample repetition
-    
-    
-    
-    ## aggregate statistics for tuning parameters
-    tune_file_ij <- as.data.frame(cbind(mtry= round(mean(tune$mtry)), 
-                                        ntree= round(mean(tune$ntree)), 
-                                        region = region_name[i],
-                                        year = set_of_years[j]))
-    
-    rm(tune)
-    
-    ## compile all years of each region
-    if (exists('tune_y') == FALSE) {
-      tune_y <- tune_file_ij
-    } else {
-      tune_y <- rbind(tune_y, tune_file_ij)
-    }
-    
-    ## aggregate statistics for variable importance
-    bands_file_ij <- as.data.frame(cbind(band= unique(imp$band),
-                                         sum= aggregate(x=list(sum = imp$Overall), by=list(band= imp$band), FUN= 'sum')$sum,
-                                         mean= aggregate(x=list(mean = imp$Overall), by=list(band= imp$band), FUN= 'mean')$mean,
-                                         sd= aggregate(x=list(sd = imp$Overall), by=list(band= imp$band), FUN= 'sd')$sd,
-                                         region= region_name[i],
-                                         year= set_of_years[j]))
-    
-    rm(imp)
-    
-    ## compile all years of each region
-    if (exists('bands_y') == FALSE) {
-      bands_y <- bands_file_ij
-    } else {
-      bands_y <- rbind(bands_y, bands_file_ij)
-    }
-    
-    
-  } ## end of years loop
-  
-  ## aggregate statistics for tuning parameters
-  tune_file_i <- as.data.frame(cbind(mtry= round(mean(tune_y$mtry)), 
-                                     ntree= round(mean(tune_y$ntree)), 
-                                     region = region_name[i]))
-  
-  ## insert into database
-  ## rf parameters
-  if (exists('tune_xx') == FALSE) {
-    tune_xx <- tune_file_i
-  } else {
-    tune_xx <- rbind(tune_xx, tune_file_i)
-  }
-  
-  ## aggregate region metrics
-  bands_file_i <- as.data.frame(cbind(band= unique(bands_y$band),
-                                      sum= aggregate(x=list(sum = as.numeric(bands_y$sum)), by=list(band= bands_y$band), FUN= 'mean')$sum,
-                                      mean= aggregate(x=list(mean = as.numeric(bands_y$mean)), by=list(band= bands_y$band), FUN= 'mean')$mean,
-                                      sd= aggregate(x=list(sd = as.numeric(bands_y$sd)), by=list(band= bands_y$band), FUN= 'mean')$sd,
-                                      region= region_name[i]))
-  
-  ## variable importance
-  if (exists('bands_xx') == FALSE) {
-    bands_xx <- bands_file_i
-  } else {
-    bands_xx <- rbind(bands_xx, bands_file_i)
-  }
-  
-  rm(tune_y, bands_y)
-  
-  print ('done :) -----------> next')
-  
-  ## clean regional variables
-  #rm (mosaic_i, samples_i, geo_coordinates, lat, lon_sin, lon_cos, hand)
-} ## end of regions loop
-
-## export tables
-write.table(bands_xx, file = './_params/bands.csv')
-write.table(tune_xx, file = './_params/rf.csv')
-print('end o/')
+## save data locally to be used 
+write.table(paramTable, file = './_aux/modelParams.csv')
+write.table(importanceTable, file = './_aux/varImportance.csv')
