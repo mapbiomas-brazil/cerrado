dc08a3d90fc14fae04c4b1691791d3a0979a7d9a
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2024-05-02 17:21:59 -0300

    Update 09_frequency.js

--- 1-general-map/09_frequency.js
+++ 1-general-map/09_frequency.js
@@ -2,78 +2,81 @@
 // frequency filter 
 // barbara.silva@ipam.org.br
 
-// set root directory 
-var root = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO8_DEV/';
+// Set root directory
+var root = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/C9-GENERAL-POST/';
+var out = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/C9-GENERAL-POST/';
 
-// set metadata 
-var version_input = '14'
-var version_out = '14';
+// Set metadata
+var inputVersion = '1';
+var outputVersion = '1';
 
-// define input file 
-var file_in = 'CERRADO_col8_gapfill_incidence_temporal_v'+version_input;
+// Define input file
+var inputFile = 'CERRADO_col9_gapfill_v4_incidence_v4_temporal_v'+inputVersion;
 
-// load classification
-var classification = ee.Image(root + file_in);
-print ('input', classification);
-
-// import mapbiomas color ramp 
+// Import MapBiomas color ramp
 var vis = {
-      min: 0,
-      max: 62,
-      palette:require('users/mapbiomas/modules:Palettes.js').get('classification7'),
-    };
+    'min': 0,
+    'max': 62,
+    'palette': require('users/mapbiomas/modules:Palettes.js').get('classification8')
+};
 
-// define the function to calc frequencies 
+// Load input classification
+var classificationInput = ee.Image(root + inputFile);
+print('Input classification', classificationInput);
+
+// Define the function to calc frequencies 
 var filterFreq = function(image) {
   // expression to get frequency
  var exp = '100*((b(0)+b(1)+b(2)+b(3)+b(4)+b(5)+b(6)+b(7)+b(8)+b(9)+b(10)' +
                 '+b(11)+b(12)+b(13)+b(14)+b(15)+b(16)+b(17)+b(18)+b(19)+b(20)' +
                 '+b(21)+b(22)+b(23)+b(24)+b(25)+b(26)+b(27)+b(28)+b(29)+b(30)' +
-                '+b(31)+b(32)+b(33)+b(34)+b(35)+b(36)+b(37))/38)';
+                '+b(31)+b(32)+b(33)+b(34)+b(35)+b(36)+b(37)+b(38))/39)';
 
-  // get per class frequency 
+  // Get per class frequency 
   var forest = image.eq(3).expression(exp);
   var savanna = image.eq(4).expression(exp);
   var wetland = image.eq(11).expression(exp);
   var grassland = image.eq(12).expression(exp);
 
-  // select pixels that were native vegetation at least 95% of the time series
+  // Select pixels that were native vegetation at least 95% of the time series
   var stable_native = ee.Image(0).where(forest
                                  .add(savanna)
                                  .add(wetland)
                                  .add(grassland)
-                                 .gte(90), 1);
+                                 .gte(95), 1);
+                                 
+  Map.addLayer (stable_native, vis, "stable_native", false);
 
-  // stabilize native class when:
-  var filtered = ee.Image(0).where(stable_native.eq(1).and(forest.gte(75).and(classification.select(['classification_2022']).neq(3))), 3)
-                            .where(stable_native.eq(1).and(wetland.gte(50)), 11)
-                            .where(stable_native.eq(1).and(savanna.gt(50)), 4)
-                            .where(stable_native.eq(1).and(grassland.gt(50)), 12);
+  // Stabilize native class when:
+  var filtered = ee.Image(0).where(stable_native.eq(1).and(forest.gte(80).and(classificationInput.select(['classification_2023']).neq(3))), 3)
+                            .where(stable_native.eq(1).and(wetland.gte(60)), 11)
+                            .where(stable_native.eq(1).and(savanna.gt(60)), 4)
+                            .where(stable_native.eq(1).and(grassland.gt(60)), 12);
 
-  // get only pixels to be filtered
+  // Get only pixels to be filtered
   filtered = filtered.updateMask(filtered.neq(0));
   
   return image.where(filtered, filtered);
 };
 
-// apply function  
-var classification_filtered = filterFreq(classification);
+// Apply function  
+var classification_filtered = filterFreq(classificationInput);
 
-// plot
-Map.addLayer(classification.select(['classification_1990']), vis, 'classification');
-Map.addLayer(classification_filtered.select(['classification_1990']), vis, 'filtered');
+// Plot
+Map.addLayer(classificationInput.select(['classification_2020']), vis, 'input classification');
+Map.addLayer(classification_filtered.select(['classification_2020']), vis, 'filtered');
 
-print ('output', classification_filtered);
+print ('Output classification', classification_filtered);
 
-// export as GEE asset
+// Export as GEE asset
 Export.image.toAsset({
     'image': classification_filtered,
-    'description': 'CERRADO_col8_gapfill_incidence_temporal_frequency_v' + version_out,
-    'assetId': root + 'CERRADO_col8_gapfill_incidence_temporal_frequency_v' + version_out,
+    'description': 'CERRADO_col9_gapfill_v4_incidence_v4_temporal_v'+inputVersion+'_frequency_v' + outputVersion,
+    'assetId': root + 'CERRADO_col9_gapfill_v4_incidence_v4_temporal_v'+inputVersion+'_frequency_v' + outputVersion,
     'pyramidingPolicy': {
         '.default': 'mode'
     },
-    'region': classification.geometry(),
+    'region': classification_filtered.geometry(),
     'scale': 30,
     'maxPixels': 1e13
 });
