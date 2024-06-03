80b21a17ea602d168573f2d5da24b28cbf44a836
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2024-06-03 09:34:59 -0300

    Update 09_frequency.js

--- 1-general-map/09_frequency.js
+++ 1-general-map/09_frequency.js
@@ -1,32 +1,33 @@
 // -- -- -- -- 09_frequency
-// frequency filter 
-// barbara.silva@ipam.org.br
+// post-processing filter: stabilize areas of native vegetation that have remained for at least 90% of the data time series
+// barbara.silva@ipam.org.br and dhemerson.costa@ipam.org.br
 
-// Set root directory
+// Import mapbiomas color schema 
+var vis = {
+    min: 0,
+    max: 62,
+    palette:require('users/mapbiomas/modules:Palettes.js').get('classification8')
+};
+
+// Set root directory 
 var root = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/C9-GENERAL-POST/';
 var out = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/C9-GENERAL-POST/';
 
 // Set metadata
-var inputVersion = '1';
-var outputVersion = '1';
+var inputVersion = '8';
+var outputVersion = '9';
 
 // Define input file
-var inputFile = 'CERRADO_col9_gapfill_v4_incidence_v4_temporal_v'+inputVersion;
-
-// Import MapBiomas color ramp
-var vis = {
-    'min': 0,
-    'max': 62,
-    'palette': require('users/mapbiomas/modules:Palettes.js').get('classification8')
-};
+var inputFile = 'CERRADO_col9_gapfill_v8_incidence_v8_temporal_v'+inputVersion;
 
 // Load input classification
 var classificationInput = ee.Image(root + inputFile);
 print('Input classification', classificationInput);
+Map.addLayer(classificationInput.select(['classification_2023']), vis, 'Input classification');
 
 // Define the function to calc frequencies 
 var filterFreq = function(image) {
-  // expression to get frequency
+  // Expression to get frequency
  var exp = '100*((b(0)+b(1)+b(2)+b(3)+b(4)+b(5)+b(6)+b(7)+b(8)+b(9)+b(10)' +
                 '+b(11)+b(12)+b(13)+b(14)+b(15)+b(16)+b(17)+b(18)+b(19)+b(20)' +
                 '+b(21)+b(22)+b(23)+b(24)+b(25)+b(26)+b(27)+b(28)+b(29)+b(30)' +
@@ -38,20 +39,20 @@ var filterFreq = function(image) {
   var wetland = image.eq(11).expression(exp);
   var grassland = image.eq(12).expression(exp);
 
-  // Select pixels that were native vegetation at least 95% of the time series
+  // Delect pixels that were native vegetation at least 90% of the time series
   var stable_native = ee.Image(0).where(forest
                                  .add(savanna)
                                  .add(wetland)
                                  .add(grassland)
-                                 .gte(95), 1);
-                                 
+                                 .gte(90), 1);
+
   Map.addLayer (stable_native, vis, "stable_native", false);
 
   // Stabilize native class when:
-  var filtered = ee.Image(0).where(stable_native.eq(1).and(forest.gte(80).and(classificationInput.select(['classification_2023']).neq(3))), 3)
+  var filtered = ee.Image(0).where(stable_native.eq(1).and(forest.gte(75)), 3)
                             .where(stable_native.eq(1).and(wetland.gte(60)), 11)
-                            .where(stable_native.eq(1).and(savanna.gt(60)), 4)
-                            .where(stable_native.eq(1).and(grassland.gt(60)), 12);
+                            .where(stable_native.eq(1).and(savanna.gt(50)), 4)
+                            .where(stable_native.eq(1).and(grassland.gt(50)), 12);
 
   // Get only pixels to be filtered
   filtered = filtered.updateMask(filtered.neq(0));
@@ -62,17 +63,14 @@ var filterFreq = function(image) {
 // Apply function  
 var classification_filtered = filterFreq(classificationInput);
 
-// Plot
-Map.addLayer(classificationInput.select(['classification_2020']), vis, 'input classification');
-Map.addLayer(classification_filtered.select(['classification_2020']), vis, 'filtered');
-
-print ('Output classification', classification_filtered);
+Map.addLayer(classification_filtered.select(['classification_2023']), vis, 'filtered');
+print('Output classification', classification_filtered);
 
 // Export as GEE asset
 Export.image.toAsset({
     'image': classification_filtered,
-    'description': 'CERRADO_col9_gapfill_v4_incidence_v4_temporal_v'+inputVersion+'_frequency_v' + outputVersion,
-    'assetId': out + 'CERRADO_col9_gapfill_v4_incidence_v4_temporal_v'+inputVersion+'_frequency_v' + outputVersion,
+    'description': inputFile + '_frequency_v' + outputVersion,
+    'assetId': out +  inputFile + '_frequency_v' + outputVersion,
     'pyramidingPolicy': {
         '.default': 'mode'
     },
